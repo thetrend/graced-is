7cca7058397b4b1b883d2c5f4697af1b9e143f44
Grace de la Mora <thetrend@users.noreply.github.com>
2024-03-26 19:21:25 -0400

    feat(theme): beginning theme work

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,24 @@
 import { Outlet } from 'react-router-dom'
 import NavLinks from './NavLinks'
+import siteBanner from '../assets/site.svg'
+import siteHeader from '../assets/siteheader.png'
 
 function Layout() {
   return (
     <>
+      <div className="inline-flex p-10">
+        <img
+          src={siteHeader}
+          style={{ minHeight: '60px', maxHeight: '300px' }}
+          alt="Laptop, stylus, and makeup brush"
+        />
+        <img
+          src={siteBanner}
+          style={{ height: '100px' }}
+          className="ml-10"
+          alt="grace d. is"
+        />
+      </div>
       <NavLinks />
       <div className="prose">
         <Outlet />
